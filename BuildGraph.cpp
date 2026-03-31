707f1790d1fb53213c03917bffcb12ae9f0392a9
arbaj-silar <silararbaj.in>
2026-03-31 09:11:17 +0530

    Building graph

---
+++ BuildGraph.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<list>
+#include<vector>
+using namespace std;
+
+class Graph {
+    int V;
+    list<int> *l;
+
+public:
+    Graph(int V) {
+        this->V = V;
+        l = new list<int> [V];
+    }
+
+    void addEdge(int u, int v) {
+        l[u].push_back(v);
+        l[v].push_back(u);
+    }
+
+    void printAdList() {
+        for(int i=0; i<V; i++) {
+            cout<<i<<" : ";
+            for(int neigh: l[i]) {
+                cout<<neigh<<" ";
+            }
+            cout<<endl;
+        }
+    }
+};
+
+int main() {
+    Graph g(5);
+
+    g.addEdge(0, 1); // 2->3
+    g.addEdge(1, 2); // 1->2
+    g.addEdge(1, 3); // 2->4
+    g.addEdge(2, 3); // 3->4
+    g.addEdge(2, 4); // 3->5
+
+    g.printAdList();
+    return 0;
+}
